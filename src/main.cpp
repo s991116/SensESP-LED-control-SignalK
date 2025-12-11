606e4d9aca23df5ac6be868c765d2a3a24f76f52
s991116 <philip.juhl@gmail.com>
2025-12-11 23:26:40 +0100

    Added Nightmode and Read only state, removed update states

--- src/main.cpp
+++ src/main.cpp
@@ -28,20 +28,59 @@ void setup() {
 
   FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
 
-  auto* strip = new LedStrip(leds, 0, 3);
-
+  auto* strip   = new LedStrip(leds, 0, 3);
   auto* stateIO = new LedStripStateIO(strip);
   auto* levelIO = new LedStripLevelIO(strip);
 
+  //
+  // --- SignalK INPUT LISTENERS ---
+  //
   auto* sk_state = new SKValueListener<bool>("electrical.switches.cabinLights.state");
   sk_state->connect_to(stateIO);
 
   auto* sk_level = new SKValueListener<int>("electrical.switches.cabinLights.level");
   sk_level->connect_to(levelIO);
 
+  //
+  // --- HENT AKTUEL VÆRDI FRA SIGNALK VED OPSTART ---
+  //
+  auto* sk_client = sensesp_app->get_signalk_client();
+
+  sk_client->set_connected_callback([stateIO, levelIO, sk_client]() {
+
+    ESP_LOGD("INIT", "Connected to SignalK -> requesting initial values...");
+
+    // Request state (bool)
+    sk_client->request_value(
+        "electrical.switches.cabinLights.state",
+        [stateIO](JsonObject obj) {
+          if (obj.containsKey("value")) {
+            bool val = obj["value"].as<bool>();
+            ESP_LOGD("INIT", "Initial state from SK: %d", val);
+            stateIO->set(val);
+          }
+        }
+    );
+
+    // Request level (int)
+    sk_client->request_value(
+        "electrical.switches.cabinLights.level",
+        [levelIO](JsonObject obj) {
+          if (obj.containsKey("value")) {
+            int lvl = obj["value"].as<int>();
+            ESP_LOGD("INIT", "Initial level from SK: %d", lvl);
+            levelIO->set(lvl);
+          }
+        }
+    );
+  });
+
+  //
+  // Start SenseESP
+  //
   sensesp_app->start();
 }
 
 void loop() {
-    event_loop()->tick();
+  event_loop()->tick();
 }
